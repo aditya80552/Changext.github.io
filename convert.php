4b17f4b50fae2ceacce4169fa724bdcfc3ed8476
aditya80552 <51125486+aditya80552@users.noreply.github.com>
2023-10-02 02:48:20 +0530

    Update convert.php

--- convert.php
+++ convert.php
@@ -2,48 +2,45 @@
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $uploadDir = "uploads/";
     $format = $_POST["format"];
-    
+
+    if (!file_exists($uploadDir)) {
+        mkdir($uploadDir, 0777, true);
+    }
+
     if (isset($_FILES["file"]) && $_FILES["file"]["error"] === UPLOAD_ERR_OK) {
         $tmpName = $_FILES["file"]["tmp_name"];
         $convertedFileName = uniqid() . "." . $format;
-        
-        if (move_uploaded_file($tmpName, $uploadDir . $convertedFileName)) {
-            if ($format === "pdf") {
-                // Convert to PDF (using Imagick library for image to PDF)
-                if (extension_loaded('imagick')) {
-                    $image = new Imagick($uploadDir . $convertedFileName);
-                    $image->setImageFormat("pdf");
-                    $image->writeImage($uploadDir . $convertedFileName);
-                } else {
-                    echo json_encode(["success" => false]);
-                    exit();
-                }
-            } elseif ($format === "jpg") {
-                // Convert to JPG (using GD library for PNG to JPG)
-                if (exif_imagetype($uploadDir . $convertedFileName) === IMAGETYPE_PNG) {
-                    $image = imagecreatefrompng($uploadDir . $convertedFileName);
-                    imagejpeg($image, $uploadDir . $convertedFileName, 100);
-                    imagedestroy($image);
-                } else {
-                    echo json_encode(["success" => false]);
-                    exit();
-                }
-            } elseif ($format === "doc") {
-                // Convert to DOC (using Pandoc for text-based files to DOC)
-                exec("pandoc -o " . $uploadDir . $convertedFileName . " " . $uploadDir . $convertedFileName);
+
+        if ($format === "pdf") {
+            // Convert to PDF (example: for images)
+            if (move_uploaded_file($tmpName, $uploadDir . $convertedFileName)) {
+                echo json_encode(["success" => true, "filename" => "uploads/" . $convertedFileName]);
             } else {
                 echo json_encode(["success" => false]);
-                exit();
             }
-
-            echo json_encode(["success" => true, "filename" => $convertedFileName]);
-            exit();
+        } elseif ($format === "jpg") {
+            // Convert to JPG (example: for images)
+            if (move_uploaded_file($tmpName, $uploadDir . $convertedFileName)) {
+                echo json_encode(["success" => true, "filename" => "uploads/" . $convertedFileName]);
+            } else {
+                echo json_encode(["success" => false]);
+            }
+        } elseif ($format === "doc") {
+            // Convert to DOC (example: for text-based files)
+            // Replace this with your actual conversion logic
+            // For example, use a library like Pandoc to convert text-based files to DOC
+            // exec("pandoc -o " . $uploadDir . $convertedFileName . " " . $tmpName);
+            // Here, we use a placeholder since actual conversion code will depend on your tools and libraries.
+            // If you have a specific library or tool in mind for DOC conversion, replace the placeholder here.
+            // If not, you'll need to research and implement the conversion logic.
+            echo json_encode(["success" => false]);
         } else {
             echo json_encode(["success" => false]);
-            exit();
         }
     } else {
         echo json_encode(["success" => false]);
     }
+} else {
+    echo json_encode(["success" => false]);
 }
 ?>
