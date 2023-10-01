4315c3afed2aeb272ba7d80c9ac96103777a79d7
aditya80552 <51125486+aditya80552@users.noreply.github.com>
2023-10-02 03:21:03 +0530

    Update convert.php

--- convert.php
+++ convert.php
@@ -10,33 +10,36 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if (isset($_FILES["file"]) && $_FILES["file"]["error"] === UPLOAD_ERR_OK) {
         $tmpName = $_FILES["file"]["tmp_name"];
         $convertedFileName = uniqid() . "." . $format;
+        $convertedFilePath = $uploadDir . $convertedFileName;
 
         if ($format === "pdf") {
             // Convert to PDF (example: for images)
-            if (move_uploaded_file($tmpName, $uploadDir . $convertedFileName)) {
-                echo json_encode(["success" => true, "filename" => "uploads/" . $convertedFileName]);
-            } else {
-                echo json_encode(["success" => false]);
+            if (move_uploaded_file($tmpName, $convertedFilePath)) {
+                header("Content-Type: application/octet-stream");
+                header("Content-Disposition: attachment; filename=\"$convertedFileName\"");
+                readfile($convertedFilePath);
+                exit();
             }
         } elseif ($format === "jpg") {
             // Convert to JPG (example: for images)
-            if (move_uploaded_file($tmpName, $uploadDir . $convertedFileName)) {
-                echo json_encode(["success" => true, "filename" => "uploads/" . $convertedFileName]);
-            } else {
-                echo json_encode(["success" => false]);
+            if (move_uploaded_file($tmpName, $convertedFilePath)) {
+                header("Content-Type: application/octet-stream");
+                header("Content-Disposition: attachment; filename=\"$convertedFileName\"");
+                readfile($convertedFilePath);
+                exit();
             }
         } elseif ($format === "doc") {
             // Convert to DOC (example: for text-based files)
             // Replace this with your actual conversion logic
             // For example, use a library like Pandoc to convert text-based files to DOC
-            // exec("pandoc -o " . $uploadDir . $convertedFileName . " " . $tmpName);
+            // exec("pandoc -o " . $convertedFilePath . " " . $tmpName);
             // Here, we use a placeholder since actual conversion code will depend on your tools and libraries.
             // If you have a specific library or tool in mind for DOC conversion, replace the placeholder here.
             // If not, you'll need to research and implement the conversion logic.
-            echo json_encode(["success" => false]);
-        } else {
-            echo json_encode(["success" => false]);
         }
+
+        // If the conversion failed or the format is not supported
+        echo json_encode(["success" => false]);
     } else {
         echo json_encode(["success" => false]);
     }
